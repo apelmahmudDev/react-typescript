d8f97dcca0388c36f1eef74f9d7e7e3e23e3516c
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-04 19:45:56 +0600

    context with typescript learned

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,26 +1,56 @@
 import React, { useState } from "react";
 import Contact from "./Contact";
 
+interface IContact {
+	name: string;
+	email: string;
+}
 const Contacts = () => {
-	const [contact, setContact] = useState("");
-	const [contactList, setContactList] = useState<string[]>([]);
+	const [contact, setContact] = useState<IContact>({
+		name: "",
+		email: "",
+	});
+	const [contactList, setContactList] = useState<IContact[]>([]);
 
 	const onClick = () => {
 		setContactList([...contactList, contact]);
-		setContact("");
+		setContact({
+			name: "",
+			email: "",
+		});
 	};
+
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setContact({ ...contact, [e.target.name]: e.target.value });
+	};
+
+	// const handleRemove = (email: string) => {
+	// 	const newContactList = contactList.filter((c) => c.email !== email);
+	// 	setContactList(newContactList);
+	// };
+
 	return (
 		<div>
-			<h1>🦸 Contact</h1>
-			<input
-				onChange={(e) => setContact(e.target.value)}
-				value={contact}
-				type="text"
-				placeholder="contact list"
-			/>
-			<button onClick={onClick}>Submit</button>
-			{contactList.map((contactName) => (
-				<Contact key={contactName} name={contactName} />
+			<h1>🦸 Contact list</h1>
+			<div className="form">
+				<input
+					value={contact.name}
+					onChange={onChange}
+					name="name"
+					placeholder="Contact Name"
+					type="text"
+				/>
+				<input
+					value={contact.email}
+					onChange={onChange}
+					name="email"
+					placeholder="Contact Email"
+					type="email"
+				/>
+				<button onClick={onClick}>Add</button>
+			</div>
+			{contactList.map((con) => (
+				<Contact key={con.name} name={con.name} email={con.email} />
 			))}
 		</div>
 	);
